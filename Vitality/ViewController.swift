5e4fde750464a27da15098daa1b1a18017366cef
Mario <mario.aguirre@me.com>
2020-06-19 18:14:14 +1200

    Initial Commit

---
+++ Vitality/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vitality
+//
+//  Created by Mario Aguirre on 19/06/20.
+//  Copyright © 2020 Mario & Simran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
