d71315d21177a8245358b7c9405f1875add27ca9
Simran Deo <simran2064@gmail.com>
2020-06-28 22:08:22 +1200

    FoodTracker Code

---
+++ Vitality/Food_Tracker/Meal.swift
@@ -0,0 +1,70 @@
+//
+//  Meal.swift
+//  food-tracker-app
+//
+//  Created by Dipansh Khandelwal on 20/10/19.
+//  Copyright © 2019 DipanshKhandelwal. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class Meal: NSObject, NSCoding {
+    //MARK: Initialization
+    init?(name: String, photo: UIImage?, calories: Int) {
+        // Initialization should fail if there is no name or if the rating is negative.
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.calories = calories
+    }
+    
+    //MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var calories: Int
+    
+    //MARK: Archiving Paths
+     
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+    
+    //MARK: Types
+     
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let calories = "calories"
+    }
+    
+    //MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(calories, forKey: PropertyKey.calories)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        // Because photo is an optional property of Meal, just use conditional cast.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        let calories = aDecoder.decodeInteger(forKey: PropertyKey.calories)
+        
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, calories: calories)
+        
+    }
+
+}
---
+++ Vitality/Food_Tracker/MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealTableViewCell.swift
+//  food-tracker-app
+//
+//  Created by Dipansh Khandelwal on 20/10/19.
+//  Copyright © 2019 DipanshKhandelwal. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var caloriesLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
